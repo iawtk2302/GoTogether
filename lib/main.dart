0f532b7016025d86392a1092bd820b07f7599e50
bakhanh113ct <84719735+bakhanh113ct@users.noreply.github.com>
2023-03-17 21:21:40 +0700

    push

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,14 @@ Future<void> main() async{
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {   
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {   
+
+    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Go Together',
